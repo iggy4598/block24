1347da7f3c2e5f6e0236021217626880573708bd
Ignacio <iggyjurado45@gmail.com>
2024-11-07 21:50:11 -0500

    add app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,33 @@
+import { useState } from "react";
 import "./App.css";
+import { puppyList } from "./data";
 
-    <div></div>
+function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
+  function handleClick() {
+    // some code here bruh
+  }
+  //console.log("puppylist: ", puppyList);
+
+  return (
+    <div className="App">
+      {puppies.map((puppy) => {
+        return (
+          <p
+            onClick={() => {
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
+      })}
+      <p>{featPupId}</p>
+    </div>
+  );
+}
+
+export default App;
