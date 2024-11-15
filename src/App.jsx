894dfca5483dbe029d7ca6499c4965958517cb68
Ignacio <iggyjurado45@gmail.com>
2024-11-15 17:25:15 -0500

    New

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ import { puppyList } from "./data";
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
   const [featPupId, setFeatPupId] = useState(null);
-
   function handleClick() {
-    // some code here bruh
+    handleClick;
   }
   //console.log("puppylist: ", puppyList);
-
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
   return (
     <div className="App">
       {puppies.map((puppy) => {
@@ -25,9 +25,19 @@ function App() {
           </p>
         );
       })}
-      <p>{featPupId}</p>
+      <p>
+        {" "}
+        {featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
+      </p>
     </div>
   );
 }
-
 export default App;
